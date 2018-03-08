17d885eae454d46d8c98f197c9f706fe604b52f6
Rens Sikma <rens@atcomputing.nl>
2018-03-08 10:15:51 +0100

    fix inconsistent security role

--- Template/project/sidebar.php
+++ Template/project/sidebar.php
@@ -1,4 +1,4 @@
-<?php if ($this->user->hasProjectAccess('ProjectEditController', 'show', $project['id'])): ?>
+<?php if ($this->user->hasProjectAccess('ExtendedMail', 'show', $project['id'])): ?>
     <li <?= $this->app->checkMenuSelection('Mail defaults') ?>>
         <?= $this->url->link(t('Extended Mail'), 'ExtendedMail', 'show',array ('plugin' => 'ExtendedMail','project_id' => $project['id'])) ?>
     </li>
